a378599a23a4a464426f5f88fd197e0ef07f0c21
lawrence nwoko <nwokolawrence6@gmail.com>
2022-11-09 10:33:45 +0100

    updated  getInformationUsing

--- src/services/transactions/type.ts
+++ src/services/transactions/type.ts
@@ -2,17 +2,16 @@ import { Field, InputType } from 'type-graphql';
 import { Length } from 'class-validator';
 
 
-@InputType({ description: 'Wallet to Wallet transfer parameters' })
-export class walletToWalletTransferInput {
-	@Length(1)
-	@Field({ description: 'Transfer amount' })
-	amount: number;
-
-	@Field({ description: 'recipient user id' })
-	recipient: string;
-
-	@Field({ description: 'Transaction pin of sender' })
-	pinNumber: number;
-}
+// @InputType({ description: 'Wallet to Wallet transfer parameters' })
+// export class walletToWalletTransferInput {
+// 	@Field({ description: 'Transfer amount' })
+// 	amount: number;
+//
+// 	@Field({ description: 'recipient user id' })
+// 	recipient: string;
+//
+// 	@Field({ description: 'Transaction pin of sender' })
+// 	pinNumber: number;
+// }
 
 
